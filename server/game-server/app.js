ba59d6db90a59912b419a1db03194ddaa433e6b6
guanzhongcai <1067716332@qq.com>
2020-05-25 11:33:40 +0800

    update game-server app & config

--- server/game-server/app.js
+++ server/game-server/app.js
@@ -1,4 +1,32 @@
-/* 
-* GZC created on 2020/5/25
-*  
-* */
+let ExpressServer = require("../shared/server/ExpressServer");
+const serverConfig = require('../../config/game-server');
+const dbAccess = require('./db/dbAccess');
+const Code = require('../shared/server/Code');
+
+const serverType = Code.ServiceType.game;
+
+let server = new ExpressServer({
+    serverType: serverType,
+    configAddress: serverConfig.configAddress,
+    listen: serverConfig.listen,
+});
+
+const discoverServers = [
+];
+
+server.InitServer(dbAccess.InitDB, discoverServers).then(async function () {
+
+    // server.AddRouter('/', require('./routes/gatewayRoute'));
+
+    server.EnableErrorHandler();
+
+}).catch(console.error);
+
+process.on('SIGINT', function () {
+
+    server.GracefulStop(dbAccess.Close);
+});
+
+module.exports = server;
+
+
