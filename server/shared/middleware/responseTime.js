efeffd38b76dd9675374d322741d1cc6f956d88a
guanzhongcai <1067716332@qq.com>
2020-06-06 21:08:18 +0800

    fix responseTime

--- server/shared/middleware/responseTime.js
+++ server/shared/middleware/responseTime.js
@@ -4,28 +4,48 @@
 const reportFormat = require('../metrics/reportFormat');
 const command = require('../metrics/command');
 const debug = require('debug')('responseTime');
+const onHeaders = require('on-headers');
 
 /**
  *
- * @param app object server entity
+ * @param server object server entity
  * @returns function
  */
-module.exports = function (app) {
+module.exports = function (server) {
 
     return function (req, res, next) {
 
-        req.__begintime__ = Date.now();
-        next();
+        const startAt = process.hrtime();
 
-        if (req.__begintime__) {
-            const span = Date.now() - req.__begintime__;
-            if (span > 1000) {
-                debug(req.originalUrl, req.__begintime__, span, '毫秒');
-                const report = reportFormat.Timeout(app._serviceId, req, span);
-                app.NotifyMonitor('/error/add', report);
+        onHeaders(res, function () {
+            const diff = process.hrtime(startAt); //[ 0, 553352843 ]
+            const span = Math.round(diff[0] * 1e3 + diff[1] * 1e-6);
+            const fn = createSetHeader();
+            fn(req, res, span);
+
+            if (span > 500) {
+                debug(req.originalUrl, span, '毫秒');
+                const report = reportFormat.Timeout(server._serviceId, req, span);
+                server.NotifyMonitor('/error/add', report);
             }
             command.record(req.originalUrl, span);
-            delete req.__begintime__;
-        }
-    }
+        });
+
+        next();
+    };
 };
+
+/**
+ * Create function to set response time header.
+ */
+function createSetHeader() {
+
+    return function setResponseHeader(req, res, time) {
+        if (res.getHeader('X-Response-Time')) {
+            return
+        }
+
+        const val = time.toFixed(3);
+        res.setHeader('X-Response-Time', val)
+    }
+}
